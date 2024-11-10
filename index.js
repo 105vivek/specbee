f9bbc1be50539241346c8f577879c6320db59427
Vivek Yadav <105vivekyadav@gmail.com>
2024-11-11 00:59:30 +0530

    js_left

--- index.js
+++ index.js
@@ -8,10 +8,16 @@ function openSpeakerDetail(speakerId) {
   btn.classList.add("speaker-modal");
   const img = document.createElement("img");
   img.src =
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyWm4woEJIoo2ICpyegi7ugjemX8euLDpflQ&s";
+    "https://s3-alpha-sig.figma.com/img/d6a6/fffd/2100d987b5946aa1df33cf0f6c34a418?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=p0ozEGQKCEzpMDFiGB0WI7e3Jy5hs~XAzkQEmXckRff9Yql3RG4oZ3qz7U-lmzlgN5l~VSp88OF3WSEHLG8hewYzujk8BfcrkQvmBJSeDEvGfDm6uDq5Hsx9cQ1cfgLQHZz10fHEBoS7H8kb73P-hWtJj2qYrDYGdR5YhEHfHT9EQehf5C90C4PZkDVQRu6FZWCVcxHr7FcTZGWaNC-LSjOSmA4KZaKs1FIk-OEuIlV9fK7KSQoo-7pSIHsF2dGTfhxvY4s8ywp-UY0qcUPSa7BHmFJKLlpd~NhfU2JEvdZGid5XK-F6O7RbD4CLUF1cc6yaUuh1WrCdQ9fM~aRMJQ__";
   const name = document.createElement("h2");
   name.textContent = "John Doe";
   const about = document.createElement("p");
+
+  // Display the modal
+  modal.style.display = "block";
+  // const left = document.querySelector("speaker-modal__content__left");
+  // const l=document.querySelector("speaker-modal__content__left_l");
+  // const r=document.querySelector("speaker-modal__content__left_r");
   about.textContent =
     "you might fetch additional data or use an array of speaker data";
   modal.append(img, name, about);
